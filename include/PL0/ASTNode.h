1f2e9c72ff529e396a922d6efb5c9727bc02484d
kitekii <tim720719@163.com>
2020-06-18 11:17:59 +0800

    define nodes

---
+++ include/PL0/ASTNode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class ASTNode
+{
+private:
+    /* data */
+public:
+    ASTNode(/* args */);
+    ~ASTNode();
+};
+
+ASTNode::ASTNode(/* args */)
+{
+}
+
+ASTNode::~ASTNode()
+{
+}
